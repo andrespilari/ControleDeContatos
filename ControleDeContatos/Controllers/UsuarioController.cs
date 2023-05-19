04955407782dfa95d7c93fba60355275ce63d274
Andre Spilari <andrespilari@hotmail.com>
2023-05-19 17:14:48 -0300

    Criando Cadastro de Usuários

---
+++ ControleDeContatos/Controllers/UsuarioController.cs
@@ -0,0 +1,48 @@
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ControleDeContatos.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+        public IActionResult Index()
+        {
+            var usuarios = _usuarioRepositorio.BuscarTodos(); // OU List<UsuarioModel> contatos = _contatoRepositorio.BuscarTodos();
+
+            return View(usuarios);
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(UsuarioModel usuario)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _usuarioRepositorio.Adicionar(usuario);
+                    TempData["MensagemSucesso"] = "Registro cadastrado com sucesso.";
+
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return View(usuario);
+            }
+            catch (System.Exception e)
+            {
+                TempData["MensagemErro"] = $"Falha ao cadastrar o registro: {e.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ ControleDeContatos/Models/UsuarioModel.cs
@@ -0,0 +1,23 @@
+﻿using ControleDeContatos.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Obrigatório preencher esse campo.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório preencher esse campo.")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Obrigatório preencher esse campo.")]
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
+        public string Email { get; set; }
+        public PerfilEnum Perfil { get; set; }
+        [Required(ErrorMessage = "Obrigatório preencher esse campo.")]
+        public string Senha { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
+    }
+}
