860930ae4e016ae203e7b3fc06a6094d7d056ff0
Andre Spilari <andrespilari@hotmail.com>
2023-08-30 10:04:56 -0300

    Recurso de alterar senha.

---
+++ ControleDeContatos/Controllers/AlterarSenhaController.cs
@@ -0,0 +1,48 @@
+﻿using ControleDeContatos.Helper;
+using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ControleDeContatos.Controllers
+{
+    public class AlterarSenhaController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        private readonly ISessao _sessao;
+
+        public AlterarSenhaController(IUsuarioRepositorio usuarioRepositorio, ISessao sessao)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+            _sessao = sessao;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(AlterarSenhaModel alterarSenhaModel)
+        {
+            try
+            {
+                UsuarioModel usuarioLogado = _sessao.BuscarSessaoDoUsuario();
+                alterarSenhaModel.id = usuarioLogado.Id;
+
+                if (ModelState.IsValid)
+                {
+                    _usuarioRepositorio.AlterarSenha(alterarSenhaModel);
+                    TempData["MensagemSucesso"] = "Senha alterar com sucesso";
+                    return View("Index", alterarSenhaModel);
+                }
+
+                return View("Index", alterarSenhaModel);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Houve um erro ao tentar alterar a senha, o erro foi: {erro.Message}";
+                return View("Index", alterarSenhaModel);
+            }
+        }
+    }
+}
---
+++ ControleDeContatos/Models/AlterarSenhaModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
+{
+    public class AlterarSenhaModel
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        public string SenhaAtual { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        public string NovaSenha { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        //comando abaixo usado para comparar os campos e nesse caso passa o nome do campo que quer comprar no caso a variavel acima "NovaSenha".
+        [Compare("NovaSenha", ErrorMessage = "Essa senha não é igual a nova senha.")]
+        public string ConfirmarNovaSenha { get; set; }
+    }
+}
